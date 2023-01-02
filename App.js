674ef49be828e8adaee1241b7002910b648d96e3
Acer <shaxzodbekbobobekov@gmail.com>
2023-01-02 10:05:20 +0500

    added input func

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [enterText, setEnterText] = useState("");
+  const [textArray, setTextArray] = useState(["salom"]);
+  const handleChange = (text) => {
+    setEnterText(text);
+  };
+
+  function addGoalHandler() {
+    setTextArray((allText) => {
+      return [...allText, enterText];
+    });
+  }
+
+  console.log(textArray);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <TextInput style={styles.textInput} onChangeText={handleChange} />
+        <Button
+          style={styles.buttonAdded}
+          title="click me"
+          onPress={addGoalHandler}
+        />
+      </View>
+      <ScrollView>
+        {textArray &&
+          textArray.map((goal) => {
+            return (
+              <Text key={goal} style={styles.texts}>
+                {goal}
+              </Text>
+            );
+          })}
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textInput: {
+    borderColor: "#000000",
+    borderWidth: 1,
+    width: "60%",
+    padding: 2.5,
+    marginRight: 4,
+  },
+  texts: {
+    color: "black",
   },
 });
